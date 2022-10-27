92f9fc778dc0518222ad8de01c6ea93329eeb777
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-10-27 11:39:55 +0100

    commit

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express()
 const port = process.env.port || 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.status(200).json({
+        "slackUsername": "Adedigba Adedotun",
+        "backend": true,
+        "age": 22,
+        "bio": "I am currently an hng9 backend engineering intern"
+    })
 })
 
 app.listen(port, () => {
