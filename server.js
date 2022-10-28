eda6509ba152555ee5a79142bcf55fdce73f2361
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-10-28 12:59:56 +0100

    added cors

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const port = process.env.port || 3000
+const port = process.env.port || 3000;
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        "slackUsername": "adedotun",
-        "backend": true,
-        "age": 22,
-        "bio": "I am currently an hng9 backend engineering intern"
-    })
-})
+  res.status(200).json({
+    slackUsername: "adedotun",
+    backend: true,
+    age: 22,
+    bio: "I am currently an hng9 backend engineering intern",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
